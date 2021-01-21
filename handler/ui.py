1e7fccea69701213879aa38b54d7f1c49a9c7df2
dcp <574881148@qq.com>
2021-01-21 15:27:44 +0800

    fix:修复了自动更新，添加了自动获取release信息

--- handler/ui.py
+++ handler/ui.py
@@ -9,6 +9,7 @@ import time
 import requests
 import os
 import configparser
+import json
 
 from core.assess import Assesser
 from core.grade import GradeObserver
@@ -21,6 +22,7 @@ from util.functions import get_cfg
 
 import settings
 
+
 WELCOME_MESSAGE = """
 *********************************************************************************
 **      #   #   ###     #       ###    #  #   ###  #     ###    ###  ####      **
@@ -29,7 +31,7 @@ WELCOME_MESSAGE = """
 **      #   #  #     #######      #    #  #  #     #     #     #     #  #      **
 **       ###    ### ##     ##  ###     #  #   ###  ##### #      ###  #   #     **
 **                            copyright@GentleCP                               **
-**                            version: {tag}                                   **
+**                            version:{tag}                                   **
 **                github: https://github.com/GentleCP/UCASHelper               **
 **                            1:course sources download                        **
 **                            2:wifi login                                     **
@@ -38,7 +40,7 @@ WELCOME_MESSAGE = """
 **                            5:query grades                                   **
 **                            q:exit                                           **
 *********************************************************************************
-""".format(tag=settings.__version__)
+"""
 
 
 class Init(object):
@@ -46,15 +48,23 @@ class Init(object):
     用于检查一切配置信息是否合理正确
     """
 
-    def __init__(self, welcome_msg):
+    def __init__(self,
+                 welcome_msg,
+                 record_path='../conf/record.ini',
+                 *args,**kwargs):
         self._logger = LogHandler("Init")
         self._welcome_msg = welcome_msg
 
-        self._record_path = settings.RECORD_PATH
+        self._record_path = record_path
         self._cfg = get_cfg(config_path=self._record_path)
 
         self._name_of_update_section = 'update_info'
         self._name_of_update_time = 'last_update_time'   # 记录上次更新的时间
+        self._name_of_tag = 'tag'  # 当前版本号
+
+        # update api info
+        self.__update_info_api = "https://api.github.com/repos/GentleCP/UCAS-Helper"
+        self.__latest_tag_api = "https://api.github.com/repos/GentleCP/UCAS-Helper/tags"
 
         self._wifiLoginer = WifiLoginer(accounts_path=settings.ACCOUNTS_PATH)
         self._downloader = Downloader(
@@ -74,33 +84,60 @@ class Init(object):
                                 user_info=settings.USER_INFO,  # 未来删除
                                 urls=settings.URLS)
 
+    def __get_tag(self):
+        '''
+        从配置文件或在线获取当前版本号
+        :return: tag, e.g. v2.3.1
+        '''
+        if not self._cfg.has_section(self._name_of_update_section):
+            self._cfg.add_section(self._name_of_update_section)
+        try:
+            local_tag = self._cfg.get(self._name_of_update_section, self._name_of_tag)
+        except configparser.NoOptionError:
+            self._logger.info('getting latest tag')
+            return json.loads(requests.get(self.__latest_tag_api).text)[0].get('name')
+        else:
+            print(local_tag)
+            if not local_tag:
+                self._logger.info('getting latest tag')
+                return json.loads(requests.get(self.__latest_tag_api).text)[0].get('name')
+            else:
+                return local_tag
+
+
+
     def _show_welcome(self):
-        print(self._welcome_msg)
+        '''
+        :return:
+        '''
+        tag = self.__get_tag()
+        print(tag)
+        print(self._welcome_msg.format(tag=tag))
 
 
-    def __check_update(self, user="GentleCP", repo="UCAS-Helper"):
+    def __check_update(self):
         '''
         check the latest code from github repo api, if detect the new version, update the demo
-        :param user:
-        :param repo:
         :return: {}, if need update, return True and latest_update_time, else return False
         '''
         self._logger.info("Checking update...")
-        api = "https://api.github.com/repos/{user}/{repo}".format(user=user, repo=repo)
+
         try:
-            latest_update_time = requests.get(api).json()["updated_at"]
+            latest_update_time = requests.get(self.__update_info_api).json()["updated_at"]
+            latest_tag = json.loads(requests.get(self.__latest_tag_api).text)[0].get('name')
         except Exception:
-            self._logger.error("checking update faild.")
-            return {
-                'need_update':False
-            }
+                self._logger.error("checking update faild.")
+                return {
+                    'need_update':False
+                }
         try:
             last_update_time = self._cfg.get(self._name_of_update_section, self._name_of_update_time)
         except (configparser.NoSectionError, configparser.NoOptionError) as e:
             self._logger.info("Available updates detected, start updating...")
             return {
                 'need_update':True,
-                'latest_update_time':latest_update_time
+                'latest_update_time':latest_update_time,
+                'latest_tag': latest_tag
             }
         else:
             if latest_update_time == last_update_time:
@@ -113,7 +150,8 @@ class Init(object):
                 self._logger.info("Available updates detected, start updating...")
                 return {
                     'need_update': True,
-                    'lastest_update_time':latest_update_time
+                    'latest_update_time':latest_update_time,
+                    'latest_tag': latest_tag
                 }
 
 
@@ -133,9 +171,15 @@ class Init(object):
                 # update complete, update the local update time.
                 if not self._cfg.has_section(self._name_of_update_section):
                     self._cfg.add_section(self._name_of_update_section)
+
+                # update latest_update_time
                 self._cfg.set(self._name_of_update_section,
                               self._name_of_update_time,
                               check_update_res['latest_update_time'])
+                # update latest_tag
+                self._cfg.set(self._name_of_update_section,
+                              self._name_of_tag,
+                              check_update_res['latest_tag'])
                 self._cfg.write(open(self._record_path, 'w'))
                 self._logger.info("Update compelte.")
 
@@ -183,8 +227,8 @@ class Init(object):
         self._cmd()
 
 
-def main():
-    init = Init(WELCOME_MESSAGE)
+def main(*args, **kwargs):
+    init = Init(welcome_msg=WELCOME_MESSAGE, *args, **kwargs)
     init.run()
 
 
